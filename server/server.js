06400dabb1ae62604cc7367a26a1ef3a8c0398da
Aman Prajapati <amanprajapati265482@gmail.com>
2026-07-12 17:51:38 +0530

    update

--- server/server.js
+++ server/server.js
@@ -98,8 +98,6 @@ const corsOptions = {
             'http://192.168.0.147:3000',
             'http://192.168.0.147:3001',
             'https://eirs-technology.vercel.app',
-            "https://eirstechnology.com",
-            "https://crm.eirstechnology.com",
            
             ...envOrigins
         ];
